4b249bfd68a6a6f3919988d8eb0ab328eb251297
SOFIIA TANCHUK <sonimavispc@gmail.com>
2026-01-28 14:39:12 +0200

    added conditional rendering for the ingredients

--- components/Main.jsx
+++ components/Main.jsx
@@ -15,6 +15,7 @@ function handleAddIngredient(formData) {
 
 return (
   <main>
+
 <form className="add-ingredient-form" action={handleAddIngredient}>
 <input
 type="text"
@@ -24,17 +25,19 @@ name="ingredient"
 />
 <button>Add ingredient</button>
 </form>
-<section>
+
+{ingredients.length > 0 && <section>
   <h2>Ingredients on hand:</h2>
 <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-<div className="get-recipe-container">
+{ingredients.length > 3 ? <div className="get-recipe-container">
 <div>
 <h3>Ready for a recipe?</h3>
 <p>Generate a recipe from your list of ingredients.</p>
 </div>
 <button>Get a recipe</button>
-</div>
-  </section>
+</div> : "It might not give great results from the chef 👩‍🍳 Please add more than 3 ingredients."}
+  </section>}
+
   </main>
 )
 }
