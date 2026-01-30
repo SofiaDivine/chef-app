913e6e29983acd38bbbdf9df263f2e9b68b820fd
SOFIIA TANCHUK <sonimavispc@gmail.com>
2026-01-30 13:13:22 +0200

    added a tocken and ai.js

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import Recipe from "./Recipe.jsx";
+import IngredientsList from "./IngredientsList.jsx";
 
 export default function Main() {
 
-const [ingredients, setIngredients] = React.useState([]);
+const [ingredients, setIngredients] = React.useState(
+["all the main spices", "pasta", "ground beef", "tomato paste"]
+)
 
-const ingredientsListItems = ingredients.map(ingredient => (
-<li key={ingredient}>{ingredient}</li>
-));
+const [recipeShown, setRecipeShown] = React.useState(false);
+
+function toggleRecipeShown() {
+  setRecipeShown(prevShown => !prevShown);
+}
 
 function handleAddIngredient(formData) {
   const newIngredient = formData.get("ingredient");
@@ -26,18 +32,9 @@ name="ingredient"
 <button>Add ingredient</button>
 </form>
 
-{ingredients.length > 0 && <section>
-  <h2>Ingredients on hand:</h2>
-<ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-{ingredients.length > 3 ? <div className="get-recipe-container">
-<div>
-<h3>Ready for a recipe?</h3>
-<p>Generate a recipe from your list of ingredients.</p>
-</div>
-<button>Get a recipe</button>
-</div> : "It might not give great results from the chef 👩‍🍳 Please add more than 3 ingredients."}
-  </section>}
+{ingredients.length > 0 && <IngredientsList ingredients={ingredients} toggleRecipeShown={toggleRecipeShown}/>}
 
+{recipeShown && <Recipe/>}
   </main>
 )
 }
